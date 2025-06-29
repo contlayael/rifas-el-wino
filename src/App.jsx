cc7b8b8537d67c888e7bdb2fafe4717272002008
Yael Contla <yael.contla@hotmail.com>
2025-06-29 00:22:05 -0600

    app.css name modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './app.css';
 import Header from './components/Header';
 import TicketGrid from './components/TicketGrid';
 import BookingModal from './components/BookingModal';
